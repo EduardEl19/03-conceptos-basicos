79ffc208e89a8debfacdc4ca0905568bcebf3756
Eduardo López <elopez40@ucol.mx>
2019-09-17 19:34:44 -0500

    Introduccion a JS

---
+++ main.js
@@ -0,0 +1,29 @@
+// Ejemplo Conceptos Básicos
+console.log ('Hola, Mundo!');
+
+var nombre='Eduardo Lopez'
+var edad='18'
+var esEstudiante = true
+var email
+var numero1 = 5
+var numero2 = 2
+
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 % numero2)
+console.log(
+    nombre + ' tiene ' + edad + ' años. ¿Es estudiante? ' + esEstudiante
+)
+
+console.log (nombre)
+console.log (edad)
+console.log (email)
+
+edad = 'veintidos'
+console.log(edad)
+
+nombre = prompt('¿Cómo te llamas?')
+
+alert('Hola ' + nombre)
